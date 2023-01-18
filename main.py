6a307468fab3dd4d7eda821e17e9723c973e0d89
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2023-01-18 19:49:56 +0000

    end guru2

--- main.py
+++ main.py
@@ -4,10 +4,13 @@ from simulation.ma_cross import run_ma_sim
 from simulation.ema_macd_start import run_ema_macd
 from dateutil import parser
 from infrastructure.collect_data import run_collection
+from simulation.ema_macd_multiProcessing import run_ema_macd
 
 if __name__ == '__main__':
     # api = OandaApi()
     # run_collection(instrumentCollection, api)
 
-    instrumentCollection.LoadInstruments("./data")
+    # instrumentCollection.LoadInstruments("./data")
+    # run_ema_macd(instrumentCollection)
+
     run_ema_macd(instrumentCollection)
