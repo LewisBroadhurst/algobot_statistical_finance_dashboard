3cd29cb5b3325871557687865e395f5080335cbe
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2023-01-18 17:33:25 +0000

    finished guru2

--- simulation/guru_tester.py
+++ simulation/guru_tester.py
@@ -9,9 +9,15 @@ NONE = 0
 def apply_take_profit(row, PROFIT_FACTOR):
     if row.SIGNAL != NONE:
         if row.SIGNAL == BUY:
-            return (row.ask_c - row.ask_o) * PROFIT_FACTOR + row.ask_c
+            if row.direction == BUY:
+                return (row.ask_c - row.ask_o) * PROFIT_FACTOR + row.ask_c
+            else:
+                return (row.ask_o - row.ask_c) * PROFIT_FACTOR + row.ask_o
         else:
-            return (row.bid_c - row.bid_o) * PROFIT_FACTOR + row.bid_c
+            if row.direction == SELL:
+                return (row.bid_c - row.bid_o) * PROFIT_FACTOR + row.ask_c
+            else:
+                return (row.bid_o - row.bid_c) * PROFIT_FACTOR + row.ask_o
     else:
         return 0.0
 
