7890084eeefb1b5cfa42bff9f0ce3361868ed2e9
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2023-01-27 18:19:21 +0000

    created custom indicator

--- oanda_trading_bot/charting/custom_indicator.py
+++ oanda_trading_bot/charting/custom_indicator.py
@@ -1,37 +1,22 @@
 import datetime as dt
 import plotly.graph_objects as go
 import pandas as pd
+from plotly.subplots import make_subplots
 
+class CustomIndicator:
 
-class LinePlot:
-
-    def __init__(self, df: pd.DataFrame):
+    def __init__(self, df: pd.DataFrame, indicator_traces: list):
         self.df_plot = df.copy()
-        self.fig = go.Figure()
-        self.create_line_plot()
-
-    def add_timestr(self):
-        """Hack for removing weekend candles"""
         self.df_plot['sTime'] = [dt.datetime.strftime(x, "s%y-%m-%d %H:%M") for x in self.df_plot.time]
-    
-    def create_line_plot(self):
-        self.add_timestr()
-        self.fig.add_trace(go.Line(
-            x=self.df_plot.sTime,
-            y=self.df_plot.mid_c,
-            mode='lines',
-            name='price'
-        ))
-        
-    def add_ema_traces(self, ema_list: list):
-        for ema in ema_list:
-            self.df_plot[f'ema_{ema}'] = self.df_plot.mid_c.ewm(span=ema, min_periods=ema).mean()
-            
+        self.fig = go.Figure()
+        self.fig = make_subplots(specs=[[{"secondary_y": True}]])
+
+        for indicator_trace in indicator_traces:
             self.fig.add_trace(go.Line(
                 x=self.df_plot.sTime,
-                y=self.df_plot[f"ema_{ema}"],
+                y=self.df_plot[f"{indicator_trace}"],
                 mode='lines',
-                name=f'{ema} EMA'
+                name=f'{indicator_trace}'
             ))
     
     def update_layout(self, width, height, nticks):
--- oanda_trading_bot/charting/indicators.py
+++ oanda_trading_bot/charting/indicators.py
@@ -18,3 +18,38 @@ def ATR(df: pd.DataFrame, n=14):
     df[f"ATR_{n}"] = tr.rolling(window=n).mean()
     
     return df
+
+def RSI(df: pd.DataFrame, n: int):
+    alpha = 1.0 / n
+    gains = df.mid_c.diff()
+
+    wins = pd.Series([ x if x >= 0 else 0.0 for x in gains ], name="wins")
+    losses = pd.Series([ x * -1 if x < 0 else 0.0 for x in gains ], name="losses")
+
+    wins_rma = wins.ewm(min_periods=n, alpha=alpha).mean()
+    losses_rma = losses.ewm(min_periods=n, alpha=alpha).mean()
+
+    rs = wins_rma / losses_rma
+
+    df[f"RSI_{n}"] = 100.0 - (100.0 / (1.0 + rs))
+    return df
+
+def MACD(df: pd.DataFrame, n_slow=26, n_fast=12, n_signal=9):
+
+    ema_long = df.mid_c.ewm(min_periods=n_slow, span=n_slow).mean()
+    ema_short = df.mid_c.ewm(min_periods=n_fast, span=n_fast).mean()
+
+    df['MACD'] = ema_short - ema_long
+    df['SIGNAL'] = df.MACD.ewm(min_periods=n_signal, span=n_signal).mean()
+    df['HIST'] = df.MACD - df.SIGNAL
+
+    return df
+
+def KeltnerChannels(df: pd.DataFrame, n_ema=20, n_atr=10):
+    df['EMA'] = df.mid_c.ewm(span=n_ema, min_periods=n_ema).mean()
+    df = ATR(df, n=n_atr)
+    c_atr = f"ATR_{n_atr}"
+    df['KeUp'] = df[c_atr] * 2 + df.EMA
+    df['KeLo'] = df.EMA - df[c_atr] * 2
+    df.drop(c_atr, axis=1, inplace=True)
+    return df
