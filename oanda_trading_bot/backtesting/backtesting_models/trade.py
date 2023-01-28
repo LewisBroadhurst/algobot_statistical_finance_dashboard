2e6cefb74ad69ad930ba95c8d46ac579c85b0fb0
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2023-01-28 23:50:09 +0000

    finnn

--- oanda_trading_bot/backtesting/backtesting_models/trade.py
+++ oanda_trading_bot/backtesting/backtesting_models/trade.py
@@ -3,22 +3,41 @@ SELL = -1
 NONE = 0
 
 class Trade:
-    def __init__(self, row, entry_price, take_profit, stop_loss):
-        self.running = True
-        self.entry_price = entry_price
-        self.take_profit = take_profit
+    def __init__(self, row):
+        self.open = True
+        self.entry_price = row.entry_price
+        self.take_profit = row.take_profit
         self.trade = row.trade
-        self.result = 0.0
-        self.start_time = row.time
-        self.end_time = row.time
+        self.pip_gain = 0.0
+        self.open_time = row.time
+        self.close_time = row.time
         self.duration = 0
 
         if self.trade == BUY:
-            self.stop_loss = entry_price - 0.003
+            self.stop_loss = row.entry_price - 0.003
         
         if self.trade == SELL:
-            self.stop_loss = entry_price + 0.003
+            self.stop_loss = row.entry_price + 0.003
+
         
+    def tp_sl_check(self, row):
+        if self.trade == 1:
+            if row.mid_l >= self.take_profit:
+                result = self.take_profit - self.entry_price
+                self.close_trade(row, result)
+            
+            if row.mid_h >= (self.stop_loss):
+                self.close_trade(row, -0.003)
+
+        if self.trade == -1:
+            if row.mid_h <= self.take_profit:
+                result = self.entry_price - self.take_profit
+                self.close_trade(row, result)
+
+            if row.mid_l >= (self.stop_loss):
+                self.close_trade(row, -0.003)
+
+
     def close_trade(self, row, result):
         self.running = False
         self.result = result
@@ -29,20 +48,3 @@ class Trade:
         
         if row.trade == SELL:
             self.close_price = row.mid_h
-        
-    def update(self, row):
-        if self.trade == BUY:
-            if row.mid_l >= self.take_profit:
-                result = self.take_profit - self.entry_price
-                self.close_trade(row, result)
-            
-            if row.mid_h >= (self.stop_loss):
-                self.close_trade(row, -0.003)
-
-        if self.trade == SELL:
-            if row.mid_h <= self.take_profit:
-                result = self.entry_price - self.take_profit
-                self.close_trade(row, result)
-
-            if row.mid_l >= (self.stop_loss):
-                self.close_trade(row, -0.003)
--- oanda_trading_bot/backtesting/strategy_runner.py
+++ oanda_trading_bot/backtesting/strategy_runner.py
@@ -1,70 +1,24 @@
 import pandas as pd
-import datetime as dt
+from backtesting.backtesting_models.trade import Trade
 
 BUY = 1
 SELL = -1
 NONE = 0
 
-def apply_take_profit(row):
-    if row.trade == NONE:
-        return 0.0
-    
-    if row.SIGNAL == BUY:
-        return row.BB_UP
-    else:
-        return row.BB_LW
 
-def apply_stop_loss(row):
-    if row.trade == NONE:
-        return 0.0
-    
-    if row.SIGNAL == BUY:
-        return row.mid_l - 0.003
-    else:
-        return row.mid_h + 0.003
-
-def apply_signals(df: pd.DataFrame, trade_confirmation: function):
-    df["SIGNAL"] = df.apply(trade_confirmation, axis=1)
-    df["TP"] = df.apply(apply_take_profit, axis=1)
-    df["SL"] = df.apply(apply_stop_loss, axis=1)
-
-def create_signals(df, time_d=1):
-    df_signals = df[df.SIGNAL != NONE].copy() 
-
-    df_signals['m5_start'] = [x + dt.timedelta(hours=time_d) for x in df_signals.time]
-
-    df_signals.drop(['time', 'mid_o', 'mid_h', 'mid_l', 'bid_o', 'bid_h', 'bid_l',
-    'ask_o', 'ask_h', 'ask_l', 'direction'], axis=1, inplace=True)
-
-    df_signals.rename(columns={
-        'bid_c' : 'start_price_BUY',
-        'ask_c' : 'start_price_SELL',
-        'm5_start' : 'time'
-    }, inplace=True)
-
-    return df_signals
+def filter_no_trades_from_df(df):
+    df_trades = df[df.trade != NONE].copy() 
+    return df_trades
 
 
 class StrategyRunner:
 
-    def __init__(self, trade_confirmation: function, df: pd.DataFrame, take_profit: int, stop_loss: int):
+    def __init__(self, trade_confirmation, df: pd.DataFrame):
         self.df = df.copy()
         self.trade_confirmation = trade_confirmation
-        self.take_profit = take_profit
-        self.stop_loss = stop_loss
-        
-        self.prepare_data()
-    
-    def prepare_data(self):
+        # self.df["trade"] = self.df.apply(trade_confirmation, axis=1)
+        self.df = filter_no_trades_from_df(self.df)
 
-        apply_signals(self.df, self.take_profit, self.trade_confirmation)
-
-        df = self.df[['time','bid_h', 'bid_l', 'ask_h', 'ask_l' ]].copy()
-        df_signals = create_signals(self.df, time_d=self.time_d)
-
-        self.merged = pd.merge(left=df_m5_slim, right=df_signals, on='time', how='left')
-        self.merged.fillna(0, inplace=True)
-        self.merged.SIGNAL = self.merged.SIGNAL.astype(int)
     
     def run_test(self):
         open_trades = []
@@ -72,13 +26,11 @@ class StrategyRunner:
 
         for _, row in self.df.iterrows():
             if row.trade != NONE:
-                open_trades.append(Trade(row, ___, ___))
+                open_trades.append(Trade(row))
         
             for ot in open_trades:
-                ot.update(row)
-                if ot.running == False:
+                ot.tp_sl_check(row)
+                if ot.open == False:
                     closed_trades.append(ot)
-            open_trades_m5 = [x for x in open_trades_m5 if x.running == True]
 
-        self.df_results = pd.DataFrame.from_dict([vars(x) for x in closed_trades]) 
-        print("Result:", self.df_results.result.sum())
+        return open_trades
