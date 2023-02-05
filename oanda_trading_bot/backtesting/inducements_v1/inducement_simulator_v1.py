5530615bc64b3b845e47c4139b645830b0704a78
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2023-02-05 11:57:59 +0000

    Including candle plot into class

--- oanda_trading_bot/backtesting/inducements_v1/inducement_simulator_v1.py
+++ oanda_trading_bot/backtesting/inducements_v1/inducement_simulator_v1.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import datetime as dt
-from charting.indicators import BollingerBands, RSI
+from charting.indicators import BollingerBands, RSI, ATR
 from charting.candle_patterns import apply_candle_props, apply_marubozu
 from charting.candle_plot import CandlePlot
+import plotly.graph_objects as go
 
 
 class Trade:
@@ -21,9 +22,13 @@ class InducementSimulatorV1:
         self.df = apply_candle_props(self.df)
         self.df = BollingerBands(self.df)
         self.df = RSI(self.df, 10)
+        self.df = ATR(self.df, 7)
         self.df["sTime"] = [dt.datetime.strftime(x, "s%y-%m-%d %H:%M") for x in self.df.time]
         self.df["OB"] = self.df.apply(apply_marubozu, axis=1)
+
         self.df_merge = None
+        self.cp = None
+        self.cp_df = None
 
         self.pdh = 0
         self.pdl = 0
@@ -31,14 +36,8 @@ class InducementSimulatorV1:
         self.sl = 0
         self.trades_today = 0
 
-        self.cp = CandlePlot(self.df)
-        self.cp.add_ema_traces([5, 20])
-        self.cp.add_indicator_traces(["BB_UP"])
 
-        self.df = self.cp.df_plot
-
-
-    def refresh_hod(self, row, df):
+    def refresh_hod_lod(self, row, df):
         if row.time.hour == 22 and row.time.minute == 0:
             if row.name - 288 < 0:
                 self.pdh = 0
@@ -47,43 +46,80 @@ class InducementSimulatorV1:
                 start_of_pd = row.name - 288
                 end_of_pd = row.name
 
-                df_hl = df.iloc[start_of_pd:end_of_pd].copy()
-                hod = df_hl.mid_h.max()
+                df_hod = df.iloc[start_of_pd:end_of_pd].copy()
+                hod = df_hod.mid_h.max()
+
+                df_lod = df.iloc[start_of_pd:end_of_pd].copy()
+                lod = df_lod.mid_h.max()
 
                 self.pdh = hod
+                self.pdl = lod
 
 
     def max_trade_check(self, row):
         if row.time.hour == 22 and row.time.minute == 0:
             self.trades_today = 0
 
+
+    def create_sim_candle_plot(self, df):
+        self.cp = CandlePlot(df)
+
+        self.cp.fig.add_trace(go.Line(
+            x=self.df_merge.sTime,
+            y=self.df_merge.tp,
+            mode='markers',
+            name='tp',
+            marker=dict(size=5, color='#00FF00')
+        ))
+        self.cp.fig.add_trace(go.Line(
+            x=self.df_merge.sTime,
+            y=self.df_merge.sl,
+            mode='markers',
+            name='sl',
+            marker=dict(size=5, color='#FF0000')
+        ))
+
+        self.cp_df = self.cp.df_plot
+
+
     def run_simulation(self):
+        # TODO: move to BE or close after e.g. 1Hr open
         open_trades = []
         closed_trades = []
 
         for _, row in self.df.iterrows():
-            self.refresh_hod(row, self.df)
+            self.refresh_hod_lod(row, self.df)
             self.max_trade_check(row)
 
-            if 8 < row.time.hour < 18:
+            if self.trades_today > 2:
+                open_trades.append(Trade(0, None, None, None, row.time))
 
-                if self.trades_today > 2:
-                    open_trades.append(Trade(0, None, None, None, row.time))
+            elif (row.mid_h - 0.00100) > self.pdh != 0 and row.direction == -1 and row.OB is True and 8 < row.time.hour < 18:
+                time = row.time
+                sl = row.mid_c + 0.0020
+                tp = row.mid_c - 0.0060
+                entry_price = row.mid_c
 
-                elif row.mid_h > self.pdh != 0 and row.direction == -1 and row.OB is True:
-                    time = row.time
-                    sl = row.mid_c + 0.0020
-                    tp = row.mid_c - 0.0060
-                    entry_price = row.mid_c
+                open_trades.append(Trade(1, entry_price, tp, sl, time))
+                self.trades_today += 1
 
-                    open_trades.append(Trade(1, entry_price, tp, sl, time))
-                    self.trades_today += 1
+            elif (row.mid_l + 0.00100) < self.pdl != 0 and row.direction == 1 and row.OB is True and 8 < row.time.hour < 18:
+                time = row.time
+                sl = row.mid_c - 0.0020
+                tp = row.mid_c + 0.0060
+                entry_price = row.mid_c
 
-                else:
-                    open_trades.append(Trade(0, None, None, None, row.time))
+                open_trades.append(Trade(-1, entry_price, tp, sl, time))
+                self.trades_today += 1
+
+            else:
+                open_trades.append(Trade(0, None, None, None, row.time))
 
         df_trades = pd.DataFrame([{'trade': s.trade, 'entry': s.entry_price, 'tp': s.tp, 'sl': s.sl, 'time': s.time}
                                   for s in open_trades])
+
         self.df_merge = pd.merge(left=self.df, right=df_trades, on=['time'])
+
+        self.create_sim_candle_plot(self.df_merge)
 
         return self.df_merge
